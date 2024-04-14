cda7e5fd3a3d018bd10fa12c5f7b18f5181a0c80
Samir Nunes da Silva <samir.silva12342@gmail.com>
2024-04-14 16:17:46 -0300

    models testing to main.py

--- src/main.py
+++ src/main.py
@@ -1,3 +1,24 @@
 from hdp_data_import import import_heart_disease_data
-from hdp_model_evaluator import HdpModelEvaluator
+from hdp_data_pipeline import HdpDataPipeline
+from hdp_many_model_evaluator import HdpManyModelEvaluator
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.svm import SVC
 
+def main():
+    X_train, X_test, y_train, y_test = import_heart_disease_data()
+    random_state = 100
+    pipeline = HdpDataPipeline()
+    models = [
+        RandomForestClassifier(n_estimators=100, random_state=random_state),
+        DecisionTreeClassifier(random_state=random_state),
+        LogisticRegression(),
+        SVC(probability=True)
+    ]
+    many_evaluator = HdpManyModelEvaluator(models, pipeline)
+    for i in many_evaluator.kfold_cross_val(X_train, y_train, threshold=0.5):
+        print(i)
+
+if __name__ == "__main__":
+    main()    
