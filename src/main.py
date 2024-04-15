d63609f483a44ed2f86fe80e8e4224f6f8d462c6
Samir Nunes da Silva <samir.silva12342@gmail.com>
2024-04-14 22:13:03 -0300

    add xgboost, update dependencies

--- src/main.py
+++ src/main.py
@@ -5,6 +5,7 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.linear_model import LogisticRegression
 from sklearn.svm import SVC
+from xgboost import XGBClassifier
 
 def main():
     X_train, X_test, y_train, y_test = import_heart_disease_data()
@@ -14,7 +15,8 @@ def main():
         RandomForestClassifier(n_estimators=100, random_state=random_state),
         DecisionTreeClassifier(random_state=random_state),
         LogisticRegression(),
-        SVC(probability=True)
+        SVC(probability=True),
+        XGBClassifier()
     ]
     many_evaluator = HdpManyModelEvaluator(models, pipeline)
     for metrics in many_evaluator.kfold_cross_val(X_train, y_train, threshold=0.5):
