f9ed728cd8329b903a590d004c52aa83e2579055
Samir Nunes da Silva <samir.silva12342@gmail.com>
2024-05-19 22:07:26 -0300

    add models.py - improved organization

--- src/evaluate.py
+++ src/evaluate.py
@@ -1,32 +1,12 @@
 from hdp_data_import import import_heart_disease_data
 from hdp_data_pipeline import HdpDataPipeline
 from hdp_many_model_evaluator import HdpManyModelEvaluator
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.tree import DecisionTreeClassifier
-from sklearn.linear_model import LogisticRegression
-from sklearn.svm import SVC
-from xgboost import XGBClassifier
 import os
 import json
 import matplotlib.pyplot as plt
+from models import *
 
-def evaluate():
-    random_state = 100
-    models = [
-        RandomForestClassifier(n_estimators=100, random_state=random_state, max_depth=3, max_leaf_nodes=10)
-        #DecisionTreeClassifier(random_state=random_state),
-        #LogisticRegression(),
-        #SVC(probability=True, random_state=random_state),
-        #XGBClassifier(random_state=random_state)
-    ]
-    names = [
-        "RandomForestBase"
-        #"DecisionTreeBase",
-        #"LogisticRegressionBase",
-        #"SVCBase",
-        #"XGBClassifierBase"
-    ]
-      
+def evaluate():      
     X_train, X_test, y_train, y_test = import_heart_disease_data()
     pipeline = HdpDataPipeline()
     many_evaluator = HdpManyModelEvaluator(models, pipeline)
---
+++ src/models.py
@@ -0,0 +1,23 @@
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.svm import SVC
+from xgboost import XGBClassifier
+
+random_state = 100
+
+models = [
+        RandomForestClassifier(n_estimators=100, random_state=random_state, max_depth=3, max_leaf_nodes=10),
+        DecisionTreeClassifier(random_state=random_state, max_depth=3, max_leaf_nodes=10),
+        LogisticRegression(),
+        SVC(probability=True, random_state=random_state),
+        XGBClassifier(random_state=random_state)
+    ]
+
+names = [
+    "RandomForestMaxDepth3MaxLeafNodes10",
+    "DecisionTreeMaxDepth3MaxLeafNodes10",
+    "LogisticRegressionBase",
+    "SVCBase",
+    "XGBClassifierBase"
+]
